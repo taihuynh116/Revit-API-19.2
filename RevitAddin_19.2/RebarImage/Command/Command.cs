eef01c8ab095d8cff45f4f7f431fcf8a0926eee6
kimtuthap2005 <kimtuthap2005@gmail.com>
2019-10-10 19:57:48 +0700

    11

---
+++ RevitAddin_19.2/RebarImage/Command/Command.cs
@@ -0,0 +1,46 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SingleData;
+using Utility;
+using Autodesk.Revit.DB.Structure;
+using Selection_FilterElement;
+namespace RebarImage
+{
+    [Transaction (TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        private RevitData revitData
+        {
+            get { return RevitData.Instance; }
+        }
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            revitData.UIApplication = commandData.Application;
+            var doc = revitData.Document;
+            var collector = new FilteredElementCollector(doc);
+            var instance = collector.WhereElementIsNotElementType();
+
+            var sel = revitData.Selection;
+            var rebarSelectionFilter = new GenericSelectionFilter(x => x is Rebar);
+            var rf = sel.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element, rebarSelectionFilter);
+            var rb = doc.GetElement(rf) as Rebar;
+
+           // var rebarCuvre = rb.GetCenterlineCurves(false,false,false,MultiplanarOption.IncludeOnlyPlanarCurves,)
+
+            TaskDialog.Show("Revit", $"{rb.Name}");
+
+
+           
+           // TaskDialog.Show("Revit", $"{revitData.Families.Count()}");
+
+            RevitDataUtil.Dispose();
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ RevitAddin_19.2/RebarImage/GenericSelectionFilter.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection_FilterElement
+{
+    //Kiểu dữ liệu định nghĩa bộ lọc đối tượng tổng quát
+    public class GenericSelectionFilter : ISelectionFilter
+    {
+        private Func<Element, bool> FilterElement;
+        public GenericSelectionFilter(Func<Element,bool> filterElement)
+        {
+            this.FilterElement = filterElement;
+        }
+        public bool AllowElement(Element elem)
+        {
+            return FilterElement(elem);
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

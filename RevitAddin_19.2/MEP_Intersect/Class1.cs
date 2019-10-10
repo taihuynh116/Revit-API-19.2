b9f6ad6432f8ed9c4d7231bdaac994c5e1bfbf1e
bimtaiht <bimtaiht@gmail.com>
2019-10-10 19:04:16 +0700

    Tạo dự án MEP

---
+++ RevitAddin_19.2/MEP_Intersect/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEP_Intersect
+{
+    public class Class1
+    {
+    }
+}
